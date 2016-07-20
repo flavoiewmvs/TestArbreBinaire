f911ae23fd645bf278661da3d49f5ac4dd8ee812
flavoiewmvs <flavoie@wmvs.ca>
2016-07-20 08:57:01 -0400

    Merge origin/tp2v1 into tp2v1
    Conflicts:
    	TP2/nbproject/project.properties

---
+++ TestABR/src/testabr/TestTris.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testabr;
+
+/**
+ *
+ * @author flavo
+ */
+public class TestTris {
+ 
+    public static void main(String[] args) {
+        int [] liste1 = {12, 4, 7, 3, 6, 8, 2, 10, 9}; 
+        
+        Tris.trierInsertion(liste1);
+    }    
+}
+
