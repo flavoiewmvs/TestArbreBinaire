b7f82692e587194df58b5cc6bb526f1a9bcf20c2
flavo <flavo@172.29.88.60>
2016-07-20 08:56:03 -0400

    Solution finale
    Solution finale

--- TestArbreBinaire/src/testarbrebinaire/TestArbreBinaire.java
+++ TestArbreBinaire/src/testarbrebinaire/TestArbreBinaire.java
@@ -24,13 +24,33 @@ public class TestArbreBinaire {
          *                     \
          *                      19
          */
-        ABR monArbre = new ABR(10);
-        monArbre.inserer(20);
-        monArbre.inserer(2);
-        monArbre.inserer(14);
-        monArbre.inserer(4);
-        monArbre.inserer(19);
-        System.out.println(monArbre.taille());
+//        ABR monArbre = new ABR(10);
+//        monArbre.inserer(20);
+//        monArbre.inserer(2);
+//        monArbre.inserer(14);
+//        monArbre.inserer(4);
+//        monArbre.inserer(19);
+//        System.out.println(monArbre.taille());
+        int test [] = {12, 4, 7, 3, 6, 8, 2, 10, 9};
+        triinsertion(test);
     }
-    
+    public static void triinsertion (int []tab){
+    int cpt;
+    int element;
+ 
+    for (int i = 1; i < tab.length ; i++)
+    {    
+        element = tab[i];
+        cpt = i - 1;
+        while (cpt >= 0 && tab[cpt] > element)
+        {
+           tab[cpt + 1] = tab[cpt];
+           cpt--;
+        }
+        tab[cpt + 1] = element;
+    }
+        System.out.println("testarbrebinaire.TestArbreBinaire.triinsertion()");
+            
+}
+
 }
