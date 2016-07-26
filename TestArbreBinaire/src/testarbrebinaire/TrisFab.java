f3c4e654ae794f0d40bc2936521dfa3e044b3053
flavo <flavo@172.29.88.60>
2016-07-26 14:36:39 -0400

    Solution finale
    Solution finale

--- TestArbreBinaire/src/testarbrebinaire/TrisFab.java
+++ TestArbreBinaire/src/testarbrebinaire/TrisFab.java
@@ -23,6 +23,7 @@ public class TrisFab {
             }
             tab[posPetit] = tab[i];
             tab[i] = valeurMin;
+            aff("i=" + i + ">>  ", tab);
         }
     }
 
@@ -31,14 +32,31 @@ public class TrisFab {
             int j = i;
             Integer x = tab[i];
             while (j != 0 && tab[j - 1].compareTo(x) > 0) {
-                 tab[j] = tab[j - 1];
-                                aff(""+i+","+j+","+x+">>",tab);
+                tab[j] = tab[j - 1];
+//                aff("" + i + "," + j + "," + x + ">>", tab);
                 --j;
             }
             tab[j] = x;
-                           aff(""+i+","+j+","+x+">>",tab);
+            aff("i=" + i + ",j=" + j + "," + ">>  ", tab);
         }
 
+    }
+
+    public static <Integer extends Comparable> void triBulle(Integer[] tab) {
+        boolean estTrié = false;
+        while (!estTrié) {
+            estTrié = true;
+            for (int i = 0; i < tab.length - 1; ++i) {
+                if (tab[i].compareTo(tab[i + 1]) > 0) {
+                    Integer x = tab[i];
+                    tab[i] = tab[i + 1];
+                    tab[i + 1] = x;
+                    estTrié = false;
+                }
+            }
+            aff(">>  ", tab);
+
+        }
     }
 
     public static <Integer extends Comparable> void aff(String string, Integer[] tab) {
@@ -50,5 +68,54 @@ public class TrisFab {
         System.out.println();
     }
 
-    
+    public static <Integer extends Comparable> void triRapide_r(Integer[] tab, int debut, int fin) {
+        int ii = 0;
+        int jj = 0;
+        if (debut < fin) {
+            Integer pivot = tab[0];
+//            Integer pivot = tab[(debut + fin) / 2];
+            ii = debut;
+            jj = fin;
+            //partition
+            while (ii < jj) {
+                while (ii < jj && tab[ii].compareTo(pivot) < 0) {
+                    ++ii;
+                }
+                               System.out.println("EB>> ii="+ii+" jj="+jj);
+                while (ii < jj && tab[jj].compareTo(pivot) >= 0) {
+                    --jj;
+                }
+                Integer temp = tab[ii];
+                System.out.println("ii="+ii+" jj="+jj+" temp="+temp);
+                tab[ii] = tab[jj];
+                tab[jj] = temp;
+//                ++ii;
+//                --jj;
+            }
+        }
+        aff("ii="+ii+"jj="+jj+" >>  ", tab);
+        triRapide_r(tab, debut, ii - 1);
+        triRapide_r(tab, ii, fin);
+    }
+
+    public static <Integer extends Comparable> void triRapide(Integer[] tab) {
+        triRapide_r(tab, 0, tab.length - 1);
+    }
+
+
+    public static void main(String[] args) {
+        Integer[] liste1 = {11, 8, 6, 3, 10, 1, 9, 5, 7, 2, 14, 4, 15, 13, 12};
+
+//        testarbrebinaire.TrisFab.quickSort(liste1, 0, liste1.length - 1);
+        testarbrebinaire.TrisFab.triRapide(liste1);
+//        testarbrebinaire.TrisFab.triBulle(liste1);
+//        testarbrebinaire.TrisFab.triselection(liste1);
+//        testarbrebinaire.TrisFab.triInsertion(liste1);
+        System.out.println("Resultat tri  ");
+        for (Integer valeur : liste1) {
+            System.out.print(valeur + ",");
+        }
+
+    }
+
 }
